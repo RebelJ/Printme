c9a3911146fecd0c4e7ca32fd3291995a5972207
reveljean <jbaptiste.revel@reseau.eseo.fr>
2020-10-23 17:30:33 +0200

    "login_register activity and connect server"

---
+++ app/src/main/java/com/example/printme/ui/app/AppConfig.java
@@ -0,0 +1,11 @@
+package com.example.printme.ui.app;
+
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.0.102/popdb/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.0.102/popdb/register.php";
+
+}
